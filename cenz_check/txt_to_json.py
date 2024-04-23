a5f541e06150012b5ce93eef4134077855e31e5d
DanielKastro <fillchapter@gmail.com>
2024-04-23 19:46:47 +0700

    update

---
+++ cenz_check/txt_to_json.py
@@ -0,0 +1,12 @@
+import json
+
+
+list = []
+with open("bad_words.txt", "r", encoding="utf-8") as file:
+    for i in file:
+        word = i.lower().split("\n")[0]
+        if word != "" or word != " ":
+            list.append(word)
+
+
+
